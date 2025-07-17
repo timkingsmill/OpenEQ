0fe3f7cc2883266479f3372ec16e1a141a6b9dbc
Tim Kingsmill <tim.kingsmill@gmail.com>
2025-07-17 15:10:18 +0930

    EQ main function added to source

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+#include "main_component.h"
+
+
+// c++ main function
+int main(int argc, char* argv[])
+{
+    // This will create the main application window and start the event loop.
+}
+
+
+//==============================================================================
+// This macro generates the main() routine that launches the app.
+//START_JUCE_APPLICATION(GuiAppApplication)
---
+++ source/main_component.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
+// directly. If you need to remain compatible with Projucer-generated builds, and
+// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
+// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
+#include <juce_gui_extra/juce_gui_extra.h>
